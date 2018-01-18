f5d22837c9de0ce1145e00365e1c3665c6dd6db8
irvan <irvanirawan1993@gmail.com>
2018-01-18 18:06:48 +0700

    install zizaco

---
+++ app/models/Permission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace models\App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    //
+}
---
+++ app/models/Role.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace models\App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    //
+}
