5dc6c7e52d84b3f04b8663f130523b9844b1a2a1
irvan <irvanirawan1993@gmail.com>
2018-01-19 20:54:25 +0700

    tambah adminlte

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+Route::get('/admin', function () {
+    return view('admin.home');
+});
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::group(['prefix' => 'admin', 'middleware' => ['role:admin']], function() {
+//     Route::get('/', 'AdminController@welcome');
+//     Route::get('/manage', ['middleware' => ['permission:manage-admins'], 'uses' => 'AdminController@manageAdmins']);
+// });
